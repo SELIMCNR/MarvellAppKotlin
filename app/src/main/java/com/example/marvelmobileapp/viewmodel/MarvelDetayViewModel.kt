7e2bd59c5b316a1fb5a486a48519e939c202264e
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2025-01-10 09:50:02 +0300

    Update MarvelDetayViewModel.kt

--- app/src/main/java/com/example/marvelmobileapp/viewmodel/MarvelDetayViewModel.kt
+++ app/src/main/java/com/example/marvelmobileapp/viewmodel/MarvelDetayViewModel.kt
@@ -1,7 +1,28 @@
 package com.example.marvelmobileapp.viewmodel
 
-class MarvelDetayViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.marvelmobileapp.model.MarvelData
+import com.example.marvelmobileapp.roomdb.MarvelDatabase
+import kotlinx.coroutines.launch
 
+class MarvelDetayViewModel(application: Application) : AndroidViewModel(application)  {
+
+    val marvelLiveData = MutableLiveData<MarvelData>()
+
+    fun roomVerisiniAl(uuid: Int){
+
+        viewModelScope.launch {
+
+            val dao = MarvelDatabase(getApplication()).marvelDao()
+            val marvelDatas = dao.getMarvel(uuid)
+            marvelLiveData.value = marvelDatas
+
+
+        }
+    }
 
 
 }

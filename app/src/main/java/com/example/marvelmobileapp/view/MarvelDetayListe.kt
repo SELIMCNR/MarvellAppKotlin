244072893ee512f67303f7c4faf4c6c4c3f3d53b
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2025-01-10 09:49:34 +0300

    Update MarvelDetayListe.kt

--- app/src/main/java/com/example/marvelmobileapp/view/MarvelDetayListe.kt
+++ app/src/main/java/com/example/marvelmobileapp/view/MarvelDetayListe.kt
@@ -5,14 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.example.marvelmobileapp.databinding.FragmentMarvelDetayListeBinding
+import com.example.marvelmobileapp.util.gorselIndir
+import com.example.marvelmobileapp.util.placeHolderYap
+import com.example.marvelmobileapp.viewmodel.MarvelDetayViewModel
 
 
 class MarvelDetayListe : Fragment() {
 
     private  var _binding:FragmentMarvelDetayListeBinding?=null
     private  val binding get() = _binding!!
-
+    private lateinit var viewModel: MarvelDetayViewModel
+    var marveluuid =0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -29,7 +34,32 @@ class MarvelDetayListe : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this)[MarvelDetayViewModel::class.java]
+
+        arguments?.let {
+             marveluuid = MarvelDetayListeArgs.fromBundle(it).marvelId
+
+        }
+        viewModel.roomVerisiniAl(marveluuid)
+
+        observeLiveData()
     }
+
+    private fun observeLiveData(){
+        viewModel.marvelLiveData.observe(viewLifecycleOwner){
+            it?.let {
+                binding.marvelIsimdetay.text = it.marvelİsim
+                binding.marvelKahramanZellikdetay.text = it.marvelKahramanÖzellikleri
+                binding.marvelKahramanyas.text = it.marvelKahramanYas
+                binding.marvelKahramanyasadigiyerdetay.text = it.marvelKahramanUlke
+                binding.marvelKahramanCinsiyet.text = it.marvelKahramanCinsiyet
+                binding.marvelImagedetay.gorselIndir(it.marvelGorsel, placeHolderYap(requireContext()))
+            }
+        }
+    }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
